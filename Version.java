2326cf665278676cd78dc8f77e35631b17600261
noeparamenos <noepositivo@gmail.com>
2025-07-11 09:03:52 +0200

    segunda

--- Version.java
+++ Version.java
@@ -1,1 +1,1 @@
-public class Test { public static void main(String[] args) { System.out.println("UNO"); } }
+public class Test { public static void main(String[] args) { System.out.println("Segunda"); } }
