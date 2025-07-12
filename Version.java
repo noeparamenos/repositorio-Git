d28687dc26828fe56aec73b70a31f81537ea7adb
noeparamenos <noepositivo@gmail.com>
2025-07-12 08:43:33 +0200

    cambios desde la terminal

--- Version.java
+++ Version.java
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 public class Test { public static void main(String[] args) { System.out.println("Tercera"); } }
+=======
+public class Test { public static void main(String[] args) { System.out.println("UNO"); } }
+>>>>>>> parent of 2326cf6 (segunda)
